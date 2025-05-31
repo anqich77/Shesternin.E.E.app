d90d67261eaaf07be2b83c03529825a319a58bec
3443-23 <mike_may99@mail.ru>
2025-05-31 14:15:05 +0300

    change color

--- app/src/main/java/com/example/shesternineeapp/MainActivity.kt
+++ app/src/main/java/com/example/shesternineeapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.shesternineeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // Массив цветов для изменения
+    private val colors = listOf(Color.BLUE, Color.RED, Color.YELLOW)
+    private var currentColorIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val colorSquare = findViewById<View>(R.id.colorSquare)
+        val colorButton = findViewById<Button>(R.id.colorButton)
+
+        colorButton.setOnClickListener {
+            currentColorIndex = (currentColorIndex + 1) % colors.size
+            colorSquare.setBackgroundColor(colors[currentColorIndex])
+        }
     }
 }
